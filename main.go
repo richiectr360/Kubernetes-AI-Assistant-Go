f6480376f1f99345ae101dcbbfc3f3837a5033a2
richiectr360 <richie.amornchotsingh@gmail.com>
2024-07-16 17:45:54 -0500

    Uploaded my complete project here

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/akhilsharma90/kubectl-assistant/cmd/cli"
+
+func main() {
+	// Call the InitAndExecute function from the cli package in the cmd folder
+	cli.InitAndExecute()
+}
